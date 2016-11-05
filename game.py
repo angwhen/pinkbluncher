41ef42721f6aa80680334cfd7af66fb939c992cc
Angel <angelmwen@gmail.com>
2016-11-05 15:37:40 -0400

    idk

--- game.py
+++ game.py
@@ -23,23 +23,17 @@ def runGame():
 	while True: #game loop
 		 for event in pygame.event.get(): # event handling loop
             if event.type == KEYDOWN:
-                # moving the piece sideways
                 if (event.key == K_LEFT or event.key == K_a):
                    
-
                 elif (event.key == K_RIGHT or event.key == K_d) :
                     
 
-                # rotating the piece (if there is room to rotate)
                 elif (event.key == K_UP or event.key == K_w):
                     
             
-                # making the piece fall faster with the down key
                 elif (event.key == K_DOWN or event.key == K_s):
                     
-
-                # move the current piece all the way down
-                elif event.key == K_SPACE:
+                elif event.key == K_SPACE: #eat
                     eat()
 			elif event.type == KEYUP:
                 if (event.key == K_LEFT or event.key == K_a):
