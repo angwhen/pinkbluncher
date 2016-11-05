871422d5dbfb48440c216b91e5542a285154b901
Angel <angelmwen@gmail.com>
2016-11-05 15:52:03 -0400

    like set up some methods

--- game.py
+++ game.py
@@ -2,7 +2,11 @@ import random, time, pygame, sys
 import numpy as np
 from pygame.locals import *
 
+WINDOWWIDTH = 500
+WINDOWHEIGHT = 700
 boardArr = np.array([10,100])
+posX = 5 #chracter position 
+posY = 0
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
@@ -19,26 +23,46 @@ def main():
 
 
 def runGame():
+	global posX, posY
 	#setup variables and stuff
+	direction = "down"
+	if direction == "right":
+		#make picture of character moving right
+	elif direction == "left":
+		#make picture of character moving left
+	elif direction == "up":
+		#pic of character looking up
+	else: #direction is down
+		#picture of character moving down
+		
 	while True: #game loop
 		 for event in pygame.event.get(): # event handling loop
             if event.type == KEYDOWN:
                 if (event.key == K_LEFT or event.key == K_a):
-                   
+					if(direction == "left"):
+						move(direction)
+					direction = "left"
+				
                 elif (event.key == K_RIGHT or event.key == K_d) :
-                    
-
+					if(direction == "right"):
+						move(direction)
+					direction = "right"
                 elif (event.key == K_UP or event.key == K_w):
-                    
-            
+                    direction == "up"
                 elif (event.key == K_DOWN or event.key == K_s):
-                    
+					if(direction == "down"):
+						move(direction)
+					direction == "down"	
                 elif event.key == K_SPACE: #eat
-                    eat()
-			elif event.type == KEYUP:
-                if (event.key == K_LEFT or event.key == K_a):
-                    movingLeft = False
-                elif (event.key == K_RIGHT or event.key == K_d):
-                    movingRight = False
-                elif (event.key == K_DOWN or event.key == K_s):
-                    movingDown = False
+                    eat(direction)
+
+def canEat(x,y):
+	#return true or false based on if can eat
+
+def eat(direction):
+	#say where the goal to eat is
+	if canEat(goal):
+		#thingsa
+		
+def move(direction):
+	global characterX
